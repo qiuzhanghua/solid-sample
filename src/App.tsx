9c81cc051f823ea8e0dde3ffd808e5350ce77a09
qiuzhanghua <qiuzhanghua@icloud.com>
2023-01-09 11:13:24 +0800

    add 404 support, redirect to Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "@solidjs/router";
-// import { lazy } from "solid-js";
-// const Home = lazy(() => import("./pages/Home"));
-// const About = lazy(() => import("./pages/About"));
+import { Navigate, Route, Routes } from "@solidjs/router";
+import { lazy } from "solid-js";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
 import Navbar from "./components/Navbar";
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
 
 export default function App () {
   return (
@@ -14,6 +14,9 @@ export default function App () {
       <Routes>
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/*">
+          <Navigate href="/" />
+        </Route>
       </Routes>
     </>
   );
