aa05c907f4b7e223bd2996d5d796fcf430839a3b
qiuzhanghua <qiuzhanghua@icloud.com>
2023-01-11 08:57:56 +0800

    Add stitches in About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,23 @@
+import { styled } from "solid-stitches";
+
+const Button = styled("button", {
+  backgroundColor: "lightblue",
+  borderRadius: "1em",
+  fontSize: "16px",
+  padding: "10px 15px",
+  "&:hover": {
+    backgroundColor: "blue"
+  },
+  color: "white"
+});
+
 export default function () {
   return (
     <>
       <div class="m-3">
         Learn SolidJS<p>邱张华</p>
       </div>
+      <Button>Stitches</Button>
     </>
   );
 }
